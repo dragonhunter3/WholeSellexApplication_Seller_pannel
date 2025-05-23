60e76e118f6dc253f7051a97ae7480751f0aeb98
dragonhunter3 <khanibloch88@gmail.com>
2025-05-24 00:51:41 +0500

    add  firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:whole_sellex_selleradmin_pannel/src/features/chat/controller/chat_controller.dart';
 import 'package:whole_sellex_selleradmin_pannel/src/features/chat/controller/select_user.dart';
-
 import 'package:whole_sellex_selleradmin_pannel/src/features/dashboard_drawer/provider/dashboard_provider.dart';
 import 'package:whole_sellex_selleradmin_pannel/src/features/product/provider/categry_provider.dart';
 import 'package:whole_sellex_selleradmin_pannel/src/features/product/provider/product_provider.dart';
 import 'package:whole_sellex_selleradmin_pannel/src/features/products/conteroller/controller.dart';
 import 'package:whole_sellex_selleradmin_pannel/src/features/settings/controller/notification_provider.dart';
-
 import 'package:whole_sellex_selleradmin_pannel/src/routes/go_route.dart';
 import 'package:whole_sellex_selleradmin_pannel/src/schedule/model/provider/scheduleprovider.dart';
-
 import 'package:whole_sellex_selleradmin_pannel/src/theme/theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "AIzaSyDyN1GuWumPgqhPQW-BY7666GGRmJidMvs",
+      authDomain: "wholesellexapplication-542be.firebaseapp.com",
+      projectId: "wholesellexapplication-542be",
+      storageBucket: "wholesellexapplication-542be.firebasestorage.app",
+      messagingSenderId: "206891758660",
+      appId: "1:206891758660:web:ca52bb037750852655b811",
+    ),
+  );
   runApp(const MyApp());
 }
 
